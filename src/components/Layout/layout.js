77207e80e89eef4c0ab8f685cb16b57cdd7a8b45
Sam Gildea <sam.c.gildea@gmail.com>
2019-06-11 19:13:37 -0400

    adding contact

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,26 +1,34 @@
 import React from "react"
-import { Link } from "gatsby";
+import { Link } from "gatsby"
 
 export default ({ children }) => (
   <>
-  <title>Sam Gildea - Developer/Designer</title>
-  <div style={{ margin: `1rem auto`, maxWidth: `100%`, padding: `0 1rem` }}>
-    <div className="fixed-header">
-    <h1 className="links">sam gildea</h1>
-    <ul>
-    <li><Link className="links link-btn" to="/">home</Link></li>
-        <li><Link className="links link-btn" to="/portfolio">portfolio</Link></li>
-        <li><Link className="links link-btn" to="/contact">contact</Link></li>
-    </ul>
+    <title>Sam Gildea - Developer/Designer</title>
+    <div style={{ margin: `1rem auto`, maxWidth: `100%`, padding: `0 1rem` }}>
+      <div className="fixed-header">
+        <h1 className="links">sam gildea</h1>
+        <ul>
+          <li>
+            <Link className="links link-btn" to="/">
+              home
+            </Link>
+          </li>
+          <li>
+            <Link className="links link-btn" to="/">
+              portfolio
+            </Link>
+          </li>
+          <li>
+            <Link className="links link-btn" to="/contact">
+              contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <br />
+      <br />
+      <hr />
+      {children}
     </div>
-    <br>
-    </br>
-    <br>
-    </br>
-    <hr></hr>
-    {children}
-  </div>
   </>
 )
-
-
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
 
-import '../styles/index.scss';
-
+import "../styles/index.scss"
 
 export default () => (
-    <Layout> 
-
-
-    </Layout>
-  )
+  <Layout>
+    <a href="https://github.com/samgildea">github</a>
+    <br />
+    <a href="https://www.linkedin.com/in/sam-gildea/">linkedin</a>
+  </Layout>
+)
